871bdb2bfc0f639cc3d50b4a47deb9c431def5dd
Jakub Głowacki <xyz.glowacki@gmail.com>
2024-06-04 17:29:03 +0200

    testing

--- car_rental_app/main_proposition.py
+++ car_rental_app/main_proposition.py
@@ -5,17 +5,21 @@ def get_rental_location():
     try:
 
         street = input(f"[1/12] Podaj ulicę odbioru: ")
-        # Add validation for street
+        # Add validation for the street
 
         postal_code = input(f"[2/12] Podaj kod pocztowy odbioru(format: XX-XXX): ")
         if not re.match(r"^\d{2}-\d{3}$", postal_code):
             raise ValueError("Nieprawidłowy kod pocztowy. Spróbuj ponownie.")
 
         city = input(f"[3/12] Podaj nazwę miasta: ")
-        # Add validation for city
+        # Add validation for the city
 
     except Exception as e:
         print(e)
+
+
+def square(n):
+    return n * n
 
 
 def main():
---
+++ car_rental_app/tests/test_main_proposition.py
@@ -0,0 +1,5 @@
+from ..main_proposition import square
+
+
+def test_square():
+    assert square(4) == 8
