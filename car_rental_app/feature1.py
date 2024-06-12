af3889137a8e44c292b8cc8b6fc954d75e35c0e8
jamgre <xyz.glowacki@gmail.com>
2024-06-12 19:38:41 +0200

    updated feature1.py

--- car_rental_app/feature1.py
+++ car_rental_app/feature1.py
@@ -3,19 +3,19 @@ import re
 
 def get_rental_location() -> tuple:
     try:
-
         street: str = input("[1/12] Podaj ulicę odbioru: ")
-        # Add validation for the street
+        if not re.match(r"^\D+$", street):
+            raise ValueError("Nieprawidłowa nazwa ulicy. Spróbuj ponownie.")
 
         postal_code: str = input("[2/12] Podaj kod pocztowy odbioru(format: XX-XXX): ")
         if not re.match(r"^\d{2}-\d{3}$", postal_code):
             raise ValueError("Nieprawidłowy kod pocztowy. Spróbuj ponownie.")
 
         city: str = input("[3/12] Podaj nazwę miasta: ")
-        # Add validation for the city
+        if not re.match(r"^\D+$", city):
+            raise ValueError("Nieprawidłowa nazwa miasta. Spróbuj ponownie.")
 
         return street, postal_code, city
-
     except Exception as e:
         print(e)
 
