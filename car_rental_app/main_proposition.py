c0e2e5e08fd22340ae28341c14894f3e8265732e
jamgre <xyz.glowacki@gmail.com>
2024-06-13 10:15:29 +0200

    minor fix main_proposition

--- car_rental_app/main_proposition.py
+++ car_rental_app/main_proposition.py
@@ -1,5 +1,6 @@
 from feature1 import get_rental_location
-from json_handler import json_user_data_writer, json_user_data_reader
+from json_handler import json_user_data_writer
+# from json_handler import json_user_data_reader
 
 
 def main():
