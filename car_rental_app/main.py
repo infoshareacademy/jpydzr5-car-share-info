b3b56c4ab4d7f176aa5570728a9620afdea2fc1e
jamgre <xyz.glowacki@gmail.com>
2024-06-14 15:31:11 +0200

    minor main.py fix

--- car_rental_app/main.py
+++ car_rental_app/main.py
@@ -80,42 +80,46 @@ def main():
                 "\n* Wybierz opcję (1/2/3): "
             )
         )
+
+        if context_menu in [1, 2, 3]:
+            if context_menu == 1:
+                show_offer()
+            if context_menu == 2:
+                rental_app()
+            if context_menu == 3:
+                context = [
+                    "1. Ulica odbioru",
+                    "2. Kod pocztowy",
+                    "3. Miasto",
+                    "4. Data rezerwacji",
+                    "5. Godzina rozpoczęcia rezerwacji",
+                    "6. Data zakończenia rezerwacji",
+                    "7. Godzina zakończenia rezerwacji",
+                    "8. Kategoria samochodu",
+                    "9. Extra",
+                    "10. Imię",
+                    "11. Nazwisko",
+                    "12. Email",
+                    "13. Telefon",
+                    "14. PESEL",
+                    "15. Numer prawa jazdy",
+                    "16. Ulica",
+                    "17. Numer lokalu",
+                    "18. Kod pocztowy",
+                    "19. Miasto",
+                    "20. Kraj",
+                ]
+                # JSON user_data_reader
+                data = json_user_data_reader(path="json/user_data.json")
+                counter = 0
+                for key, value in data.items():
+                    print(f"   * {context[counter]}: {value}")
+                    counter += 1
+        else:
+            raise ValueError
+
     except ValueError:
         print("Nieprawidłowa opcja. Spróbuj ponownie.")
-
-    if context_menu == 1:
-        show_offer()
-    if context_menu == 2:
-        rental_app()
-    if context_menu == 3:
-        context = [
-            "1. Ulica odbioru",
-            "2. Kod pocztowy",
-            "3. Miasto",
-            "4. Data rezerwacji",
-            "5. Godzina rozpoczęcia rezerwacji",
-            "6. Data zakończenia rezerwacji",
-            "7. Godzina zakończenia rezerwacji",
-            "8. Kategoria samochodu",
-            "9. Extra",
-            "10. Imię",
-            "11. Nazwisko",
-            "12. Email",
-            "13. Telefon",
-            "14. PESEL",
-            "15. Numer prawa jazdy",
-            "16. Ulica",
-            "17. Numer lokalu",
-            "18. Kod pocztowy",
-            "19. Miasto",
-            "20. Kraj",
-        ]
-        # JSON user_data_reader
-        data = json_user_data_reader(path="json/user_data.json")
-        counter = 0
-        for key, value in data.items():
-            print(f"   * {context[counter]}: {value}")
-            counter += 1
 
 
 if __name__ == "__main__":
