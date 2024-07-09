d31b33969e5aeaeaf798e8ed8a6d557a8a51ae6e
jamgre <xyz.glowacki@gmail.com>
2024-07-09 11:21:12 +0200

    update

--- car_rental/rentalApp/summary_10.py
+++ car_rental/rentalApp/summary_10.py
@@ -2,17 +2,17 @@ import sqlite3
 
 with sqlite3.connect("../db.sqlite3") as conn:
     cur = conn.cursor()
-    # cur.execute(
-    #     """
-    #     SELECT *
-    #     FROM preorders p
-    #     WHERE order_status = 'confirmed'
-    #     ORDER BY id DESC
-    #     LIMIT 1;
-    #     """
-    # )
+    cur.execute(
+        """
+        SELECT *
+        FROM preorders p
+        WHERE order_status = 'confirmed'
+        ORDER BY id DESC
+        LIMIT 1;
+        """
+    )
 
-    cur.execute("SELECT * FROM preorders")
+    # cur.execute("SELECT * FROM preorders")
     result = cur.fetchall()
     for row in result:
         print(row)
