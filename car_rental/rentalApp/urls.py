96dbc645477b0a01fc529db7f0f32de5d7d9aec5
jamgre <xyz.glowacki@gmail.com>
2024-07-14 16:46:29 +0200

    added web interface

---
+++ car_rental/rentalApp/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+
+from . import views
+
+# app_name = "rentalApp"
+urlpatterns = [
+    path("", views.to_home, name="to_home"),
+    path("home/", views.home, name="home"),
+    path("rent/", views.rent, name="rent"),
+]
--- car_rental/rentalApp/views.py
+++ car_rental/rentalApp/views.py
@@ -1,3 +1,21 @@
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
 
+
 # Create your views here.
+def to_home(request):
+    return HttpResponseRedirect("home")
+
+
+def home(request):
+    return render(
+        request,
+        "rentalApp/home.html",
+    )
+
+
+def rent(request):
+    return render(
+        request,
+        "rentalApp/rent.html",
+    )
