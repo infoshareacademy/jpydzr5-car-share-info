878acbefb695abbc8448a505192193dafd6d077d
jamgre <xyz.glowacki@gmail.com>
2024-07-26 11:02:57 +0200

    unused code cleared

--- car_rental/rentalApp/forms.py
+++ car_rental/rentalApp/forms.py
@@ -2,34 +2,6 @@ from django import forms
 
 from .models import Contact
 
-# class ContactForm(forms.Form):
-#     firstname = forms.CharField(
-#         label="Imię",
-#         error_messages={
-#             "required": "To pole jest wymagane.",
-#         },
-#         widget=forms.TextInput(attrs={"class": "form-control"}),
-#         # required=False,
-#     )
-#     lastname = forms.CharField(
-#         label="Nazwisko",
-#         error_messages={"required": "To pole jest wymagane."},
-#         widget=forms.TextInput(attrs={"class": "form-control"}),
-#         # required=False,
-#     )
-#     comment = forms.CharField(
-#         label="Wiadomość",
-#         widget=forms.Textarea(attrs={"class": "form-control"}),
-#         error_messages={"required": "To pole jest wymagane."},
-#         # required=False,
-#     )
-#     email_address = forms.EmailField(
-#         label="Adres Email",
-#         error_messages={"required": "Niepoprawny adres email."},
-#         widget=forms.EmailInput(attrs={"class": "form-control"}),
-#         # required=False,
-#     )
-
 
 class ContactForm(forms.ModelForm):
     class Meta:
@@ -53,9 +25,7 @@ class ContactForm(forms.ModelForm):
             "message": {"required": "To pole jest wymagane."},
             "email": {"required": "Wprowadź poprawny adres email."},
         }
-        # BUG: Required field upon deletion allows to submit the form.
 
 
 # TODO: Client-side required field deletion triggers the display of error messages. It should override default errors.
-# TODO: ModelForm with FormView
-# TODO: Str representation to modify object
+# TODO: Str representation to modify object.
--- car_rental/rentalApp/views.py
+++ car_rental/rentalApp/views.py
@@ -25,53 +25,15 @@ def rent(request):
     )
 
 
-# TODO: BRACKETS
 class ContactView(FormView):
     form_class = ContactForm
     template_name = "rentalApp/contact.html"
     success_url = "/contact/send/"
-    # success_url = reverse("rentalApp:send")
 
     def form_valid(self, form):
         form.save()
         return super().form_valid(form)
 
 
-# TODO: Code Below i.e contact, send
-
-
-# def contact(request):
-#     if request.method == "POST":
-#         form = ContactForm(request.POST)
-#         if form.is_valid():
-#             # data = form.cleaned_data  # Storing Cleaned Data
-#             # form.save()
-#             return render(
-#                 request,
-#                 "rentalApp/contact.html",
-#                 {
-#                     "form": form,
-#                 },
-#             )
-#         else:
-#             pass  # Handle Errors In Views
-#     else:
-#         form = ContactForm()  # Client Side Form Validation
-
-#     return render(
-#         request,
-#         "rentalApp/contact.html",
-#         {
-#             "form": form,
-#         },
-#     )
-
-
 def send(request):
-    # if request.method == "POST":
     return render(request, "rentalApp/send.html")
-    # if request.method == "GET":
-    # return HttpResponseRedirect(reverse("rentalApp:contact"))
-
-
-# TODO: New contact, send
